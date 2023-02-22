d4e5336057a647f3628309ec892b2627f753beba
NAmaral <26823958+N-Amaral@users.noreply.github.com>
2023-02-22 16:16:09 +0000

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Map from "@arcgis/core/Map";
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
